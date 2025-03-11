27972cceb715910adb8478f8815c784b6470c1db
TOURNIER Ludovic <ludovic.tournier@soprasteria.com>
2025-03-11 16:44:23 +0100

    Evols objets trouves

--- app/src/main/java/com/sdis/bilan/lsf/police/DocumentsPoliceActivity.java
+++ app/src/main/java/com/sdis/bilan/lsf/police/DocumentsPoliceActivity.java
@@ -1,18 +1,88 @@
 package com.sdis.bilan.lsf.police;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
+import com.sdis.bilan.lsf.R;
 import com.sdis.bilan.lsf.databinding.DocumentsBinding;
 
 public class DocumentsPoliceActivity extends BasePoliceActivity {
 
     private DocumentsBinding documentsBinding;
 
+    private ImageView carteIdentiteView;
+    private ImageView carteVitaleView;
+    private ImageView passeportView;
+    private ImageView permisView;
+    private ImageView carteBancaireView;
+
+    private boolean isCarteIdentiteSelected = false;
+    private boolean isCarteVitaleSelected = false;
+    private boolean isPasseportSelected = false;
+    private boolean isPermisSelected = false;
+    private boolean isCarteBancaireSelected = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         documentsBinding = DocumentsBinding.inflate(getLayoutInflater());
         setContentView(documentsBinding.getRoot());
+
+        carteIdentiteView = findViewById(R.id.carte_identite_elec);
+        carteVitaleView = findViewById(R.id.carte_vitale);
+        passeportView = findViewById(R.id.passeport);
+        permisView = findViewById(R.id.permis);
+        carteBancaireView = findViewById(R.id.carte_bancaire);
     }
 
+    public void onClickCarteIdentiteElec(View view) {
+        if (isCarteIdentiteSelected) {
+            carteIdentiteView.setImageResource(R.drawable.carte_identite_elec);
+            isCarteIdentiteSelected = false;
+        } else {
+            carteIdentiteView.setImageResource(R.drawable.carte_identite_elec_select);
+            isCarteIdentiteSelected = true;
+        }
+    }
+
+    public void onClickCarteVitale(View view) {
+        if (isCarteVitaleSelected) {
+            carteVitaleView.setImageResource(R.drawable.carte_vitale);
+            isCarteVitaleSelected = false;
+        } else {
+            carteVitaleView.setImageResource(R.drawable.carte_vitale_select);
+            isCarteVitaleSelected = true;
+        }
+    }
+
+    public void onClickPasseport(View view) {
+        if (isPasseportSelected) {
+            passeportView.setImageResource(R.drawable.passeport);
+            isPasseportSelected = false;
+        } else {
+            passeportView.setImageResource(R.drawable.passeport_select);
+            isPasseportSelected = true;
+        }
+    }
+
+    public void onClickPermis(View view) {
+        if (isPermisSelected) {
+            permisView.setImageResource(R.drawable.permis);
+            isPermisSelected = false;
+        } else {
+            permisView.setImageResource(R.drawable.permis_select);
+            isPermisSelected = true;
+        }
+    }
+
+    public void onClickCarteBancaire(View view) {
+        if (isCarteBancaireSelected) {
+            carteBancaireView.setImageResource(R.drawable.carte_bancaire);
+            isCarteBancaireSelected = false;
+        } else {
+            carteBancaireView.setImageResource(R.drawable.carte_bancaire_select);
+            isCarteBancaireSelected = true;
+        }
+    }
 }
--- app/src/main/java/com/sdis/bilan/lsf/police/PortefeuillePoliceActivity.java
+++ app/src/main/java/com/sdis/bilan/lsf/police/PortefeuillePoliceActivity.java
@@ -1,7 +1,12 @@
 package com.sdis.bilan.lsf.police;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.sdis.bilan.lsf.R;
@@ -15,6 +20,20 @@ public class PortefeuillePoliceActivity extends BasePoliceActivity {
 
     private double argent = 10;
 
+    private ImageView carteIdentiteView;
+    private ImageView carteVitaleView;
+    private ImageView permisView;
+    private ImageView carteBancaireView;
+
+    private boolean isCarteIdentiteSelected = false;
+    private boolean isCarteVitaleSelected = false;
+    private boolean isPermisSelected = false;
+    private boolean isCarteBancaireSelected = false;
+
+    private ImageView portefeuilleView;
+    private ImageView colorPicker;
+    private Bitmap bitmap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +41,29 @@ public class PortefeuillePoliceActivity extends BasePoliceActivity {
         setContentView(portefeuilleBinding.getRoot());
 
         argentView = findViewById(R.id.argent);
+
+        carteIdentiteView = findViewById(R.id.carte_identite_elec);
+        carteVitaleView = findViewById(R.id.carte_vitale);
+        permisView = findViewById(R.id.permis);
+        carteBancaireView = findViewById(R.id.carte_bancaire);
+
+        portefeuilleView = findViewById(R.id.portefeuille);
+
+        colorPicker = findViewById(R.id.colorPicker);
+
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.color_picker);
+        colorPicker.setOnTouchListener((v, event) -> {
+            if (event.getAction() == MotionEvent.ACTION_MOVE || event.getAction() == MotionEvent.ACTION_DOWN) {
+                int x = (int) event.getX();
+                int y = (int) event.getY();
+
+                if (x >= 0 && x < bitmap.getWidth() && y >= 0 && y < bitmap.getHeight()) {
+                    int color = bitmap.getPixel(x, y);
+                    portefeuilleView.setColorFilter(color, PorterDuff.Mode.SRC_IN);
+                }
+            }
+            return true;
+        });
     }
 
     public void monter(View v) {
@@ -35,5 +77,45 @@ public class PortefeuillePoliceActivity extends BasePoliceActivity {
             argentView.setText(argent + "€");
         }
 
+    }
+
+    public void onClickCarteIdentiteElec(View view) {
+        if (isCarteIdentiteSelected) {
+            carteIdentiteView.setImageResource(R.drawable.carte_identite_elec);
+            isCarteIdentiteSelected = false;
+        } else {
+            carteIdentiteView.setImageResource(R.drawable.carte_identite_elec_select);
+            isCarteIdentiteSelected = true;
+        }
+    }
+
+    public void onClickCarteVitale(View view) {
+        if (isCarteVitaleSelected) {
+            carteVitaleView.setImageResource(R.drawable.carte_vitale);
+            isCarteVitaleSelected = false;
+        } else {
+            carteVitaleView.setImageResource(R.drawable.carte_vitale_select);
+            isCarteVitaleSelected = true;
+        }
+    }
+
+    public void onClickPermis(View view) {
+        if (isPermisSelected) {
+            permisView.setImageResource(R.drawable.permis);
+            isPermisSelected = false;
+        } else {
+            permisView.setImageResource(R.drawable.permis_select);
+            isPermisSelected = true;
+        }
+    }
+
+    public void onClickCarteBancaire(View view) {
+        if (isCarteBancaireSelected) {
+            carteBancaireView.setImageResource(R.drawable.carte_bancaire);
+            isCarteBancaireSelected = false;
+        } else {
+            carteBancaireView.setImageResource(R.drawable.carte_bancaire_select);
+            isCarteBancaireSelected = true;
+        }
     }
 }
