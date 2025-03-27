10c67f6458de866c87cc55be0a42d3f656293a18
TOURNIER Ludovic <ludovic.tournier@soprasteria.com>
2025-03-27 13:19:25 +0100

    Add moto

--- app/src/main/java/com/sdis/bilan/lsf/police/ClefsPoliceActivity.java
+++ app/src/main/java/com/sdis/bilan/lsf/police/ClefsPoliceActivity.java
@@ -19,23 +19,26 @@ public class ClefsPoliceActivity extends BasePoliceActivity {
     private ClefsBinding clefsBinding;
 
     private ImageButton maisonView;
-    private ImageButton voitureView;
+    private ImageButton vehiculeView;
 
     private boolean isMaisonSelected = false;
-    private boolean isVoitureSelected = false;
+    private boolean isVehiculeSelected = false;
+
+    private ImageButton vehiculePrecedentView;
+    private ImageButton vehiculeSuivantView;
 
     private ImageView marqueView;
     private TextView marqueTexteView;
 
-    private ImageButton precedentView;
-    private ImageButton suivantView;
+    private ImageButton marquePrecedenteView;
+    private ImageButton marqueSuivanteView;
 
     private final Handler handler = new Handler(Looper.getMainLooper());
 
     private final Runnable incrementRunnableUp = new Runnable() {
         @Override
         public void run() {
-            marqueSuivante(suivantView);
+            marqueSuivante(marqueSuivanteView);
             handler.postDelayed(this, 50);
         }
     };
@@ -43,21 +46,27 @@ public class ClefsPoliceActivity extends BasePoliceActivity {
     private final Runnable incrementRunnableDown = new Runnable() {
         @Override
         public void run() {
-            marquePrecedente(precedentView);
+            marquePrecedente(marquePrecedenteView);
             handler.postDelayed(this, 50);
         }
     };
 
-    private final List<Integer> listeMarques = List.of(R.drawable.aixam, R.drawable.alfa_romeo, R.drawable.alpine, R.drawable.aston_martin, R.drawable.audi,
-            R.drawable.bentley, R.drawable.bmw, R.drawable.bugatti, R.drawable.buick, R.drawable.cadillac, R.drawable.chery, R.drawable.chevrolet
-            , R.drawable.chrysler, R.drawable.citroen, R.drawable.corvette, R.drawable.cupra, R.drawable.dacia, R.drawable.daewoo, R.drawable.daf, R.drawable.daihatsu
-            , R.drawable.datsun, R.drawable.dodge, R.drawable.ds, R.drawable.ferrari, R.drawable.fiat, R.drawable.ford, R.drawable.general_motors
+    private final List<Integer> listeVehicules = List.of(R.drawable.voiture, R.drawable.moto);
+    int vehiculeSelected = 0;
+
+    private final List<Integer> listeMarquesVoiture = List.of(R.drawable.aixam, R.drawable.alfa_romeo, R.drawable.alpine, R.drawable.aston_martin, R.drawable.audi,
+            R.drawable.bellier, R.drawable.bentley, R.drawable.bmw, R.drawable.bugatti, R.drawable.buick, R.drawable.cadillac, R.drawable.casalini, R.drawable.chatenet
+            , R.drawable.chery, R.drawable.chevrolet, R.drawable.chrysler, R.drawable.citroen, R.drawable.corvette, R.drawable.cupra, R.drawable.dacia, R.drawable.daewoo,
+            R.drawable.daf, R.drawable.daihatsu, R.drawable.datsun, R.drawable.dodge, R.drawable.ds, R.drawable.ferrari, R.drawable.fiat, R.drawable.ford, R.drawable.general_motors
             , R.drawable.genesis, R.drawable.gmc, R.drawable.honda, R.drawable.hummer, R.drawable.hyundai, R.drawable.infiniti, R.drawable.isuzu, R.drawable.iveco
-            , R.drawable.jaguar, R.drawable.jeep, R.drawable.kia, R.drawable.lada, R.drawable.lamborghini, R.drawable.lancia, R.drawable.land_rover
+            , R.drawable.jaguar, R.drawable.jeep, R.drawable.jiayuan, R.drawable.kia, R.drawable.lada, R.drawable.lamborghini, R.drawable.lancia, R.drawable.land_rover
             , R.drawable.lexus, R.drawable.ligier, R.drawable.lotus, R.drawable.man, R.drawable.maserati, R.drawable.mazda, R.drawable.mc_laren, R.drawable.mercedes
-            , R.drawable.mg, R.drawable.mini, R.drawable.mitsubishi, R.drawable.nissan, R.drawable.opel, R.drawable.peugeot, R.drawable.porsche
+            , R.drawable.mg, R.drawable.microcar, R.drawable.mini, R.drawable.mitsubishi, R.drawable.nissan, R.drawable.opel, R.drawable.peugeot, R.drawable.porsche
             , R.drawable.renault, R.drawable.rolls_royce, R.drawable.rolls_royce, R.drawable.saab, R.drawable.scania, R.drawable.seat, R.drawable.skoda, R.drawable.smart
             , R.drawable.ssangyong, R.drawable.subaru, R.drawable.suzuki, R.drawable.tesla, R.drawable.toyota, R.drawable.volkswagen, R.drawable.volvo);
+    private final List<Integer> listeMarquesMoto = List.of(R.drawable.aprilia, R.drawable.bmw, R.drawable.ducati, R.drawable.harley_davidson, R.drawable.honda
+            , R.drawable.kawasaki, R.drawable.ktm, R.drawable.mash, R.drawable.mbk, R.drawable.moto_guzzi, R.drawable.peugeot, R.drawable.piaggio, R.drawable.suzuki
+            , R.drawable.triumph, R.drawable.vespa, R.drawable.yamaha);
     int marqueSelected = 0;
 
     @Override
@@ -67,32 +76,35 @@ public class ClefsPoliceActivity extends BasePoliceActivity {
         setContentView(clefsBinding.getRoot());
 
         maisonView = findViewById(R.id.maison);
-        voitureView = findViewById(R.id.voiture);
+        vehiculeView = findViewById(R.id.vehicule);
 
         marqueView = findViewById(R.id.marque);
         marqueTexteView = findViewById(R.id.marqueTexte);
 
-        precedentView = findViewById(R.id.marque_precedente);
-        suivantView = findViewById(R.id.marque_suivante);
+        vehiculePrecedentView = findViewById(R.id.vehicule_precedent);
+        vehiculeSuivantView = findViewById(R.id.vehicule_suivant);
 
-        suivantView.setOnLongClickListener(v -> {
+        marquePrecedenteView = findViewById(R.id.marque_precedente);
+        marqueSuivanteView = findViewById(R.id.marque_suivante);
+
+        marqueSuivanteView.setOnLongClickListener(v -> {
             handler.post(incrementRunnableUp);
             return true;
         });
 
-        suivantView.setOnTouchListener((v, event) -> {
+        marqueSuivanteView.setOnTouchListener((v, event) -> {
             if (event.getAction() == MotionEvent.ACTION_UP || event.getAction() == MotionEvent.ACTION_CANCEL) {
                 handler.removeCallbacks(incrementRunnableUp);
             }
             return false;
         });
 
-        precedentView.setOnLongClickListener(v -> {
+        marquePrecedenteView.setOnLongClickListener(v -> {
             handler.post(incrementRunnableDown);
             return true;
         });
 
-        precedentView.setOnTouchListener((v, event) -> {
+        marquePrecedenteView.setOnTouchListener((v, event) -> {
             if (event.getAction() == MotionEvent.ACTION_UP || event.getAction() == MotionEvent.ACTION_CANCEL) {
                 handler.removeCallbacks(incrementRunnableDown);
             }
@@ -110,42 +122,99 @@ public class ClefsPoliceActivity extends BasePoliceActivity {
         }
     }
 
-    public void selectVoiture(View v) {
-        if (isVoitureSelected) {
-            voitureView.setBackgroundResource(0);
-            isVoitureSelected = false;
+    public void selectVehicule(View v) {
+        if (isVehiculeSelected) {
+            vehiculeView.setBackgroundResource(0);
+            isVehiculeSelected = false;
         } else {
-            voitureView.setBackgroundResource(R.drawable.rounded_light_gray_background);
-            isVoitureSelected = true;
+            vehiculeView.setBackgroundResource(R.drawable.rounded_light_gray_background);
+            isVehiculeSelected = true;
+        }
+    }
+
+    public void vehiculePrecedent(View v) {
+        if (vehiculeSelected - 1 != -1) {
+            vehiculeSelected -= 1;
+            vehiculeView.setImageResource(listeVehicules.get(vehiculeSelected));
+
+            marqueSelected = 0;
+            marqueView.setImageResource(listeMarquesVoiture.get(marqueSelected));
+            marqueTexteView.setText(getMarqueTexte(listeMarquesVoiture.get(marqueSelected)));
+            marquePrecedenteView.setImageResource(0);
+            marqueSuivanteView.setImageResource(R.drawable.suivant);
+
+            vehiculeSuivantView.setImageResource(R.drawable.suivant);
+
+            if (vehiculeSelected == 0) {
+                vehiculePrecedentView.setImageResource(0);
+            }
+        }
+    }
+
+    public void vehiculeSuivant(View v) {
+        if (vehiculeSelected + 1 != listeVehicules.size()) {
+            vehiculeSelected += 1;
+            vehiculeView.setImageResource(listeVehicules.get(vehiculeSelected));
+
+            marqueSelected = 0;
+            marqueView.setImageResource(listeMarquesMoto.get(marqueSelected));
+            marqueTexteView.setText(getMarqueTexte(listeMarquesMoto.get(marqueSelected)));
+            marquePrecedenteView.setImageResource(0);
+            marqueSuivanteView.setImageResource(R.drawable.suivant);
+
+            vehiculePrecedentView.setImageResource(R.drawable.precedent);
+
+            if (vehiculeSelected == listeVehicules.size() - 1) {
+                vehiculeSuivantView.setImageResource(0);
+            }
         }
     }
 
     public void marquePrecedente(View v) {
         if (marqueSelected - 1 != -1) {
             marqueSelected -= 1;
-            marqueView.setImageResource(listeMarques.get(marqueSelected));
-            marqueTexteView.setText(getMarqueTexte(listeMarques.get(marqueSelected)));
+            if (vehiculeSelected == 0) {
+                marqueView.setImageResource(listeMarquesVoiture.get(marqueSelected));
+                marqueTexteView.setText(getMarqueTexte(listeMarquesVoiture.get(marqueSelected)));
+            } else {
+                marqueView.setImageResource(listeMarquesMoto.get(marqueSelected));
+                marqueTexteView.setText(getMarqueTexte(listeMarquesMoto.get(marqueSelected)));
+            }
 
-            suivantView.setImageResource(R.drawable.suivant);
+            marqueSuivanteView.setImageResource(R.drawable.suivant);
 
             if (marqueSelected == 0) {
-                precedentView.setImageResource(0);
+                marquePrecedenteView.setImageResource(0);
             }
         }
     }
 
     public void marqueSuivante(View v) {
-        if (marqueSelected + 1 != listeMarques.size()) {
-            marqueSelected += 1;
-            marqueView.setImageResource(listeMarques.get(marqueSelected));
-            marqueTexteView.setText(getMarqueTexte(listeMarques.get(marqueSelected)));
+        if (vehiculeSelected == 0) {
+            if (marqueSelected + 1 != listeMarquesVoiture.size()) {
+                marqueSelected += 1;
+                marqueView.setImageResource(listeMarquesVoiture.get(marqueSelected));
+                marqueTexteView.setText(getMarqueTexte(listeMarquesVoiture.get(marqueSelected)));
+                marquePrecedenteView.setImageResource(R.drawable.precedent);
 
-            precedentView.setImageResource(R.drawable.precedent);
+                if (marqueSelected == listeMarquesVoiture.size() - 1) {
+                    marqueSuivanteView.setImageResource(0);
+                }
+            }
+        } else {
+            if (marqueSelected + 1 != listeMarquesMoto.size()) {
+                marqueSelected += 1;
+                marqueView.setImageResource(listeMarquesMoto.get(marqueSelected));
+                marqueTexteView.setText(getMarqueTexte(listeMarquesMoto.get(marqueSelected)));
 
-            if (marqueSelected == listeMarques.size() - 1) {
-                suivantView.setImageResource(0);
+                marquePrecedenteView.setImageResource(R.drawable.precedent);
+
+                if (marqueSelected == listeMarquesMoto.size() - 1) {
+                    marqueSuivanteView.setImageResource(0);
+                }
             }
         }
+
     }
 
     private String getMarqueTexte(int marque) {
